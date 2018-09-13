693c5f352f0310eabfde5cecb28f8a74d7610c37
FIKO665 <41846243+FIKO665@users.noreply.github.com>
2018-09-13 08:48:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -332,10 +332,10 @@ if (message.content.startsWith('@support')){
 });
    
    
-client.on('guildCreate', guild => {
-  client.channels.get("489635595551899648").send("Woops new server ✅")
+   client.on('guildCreate', guild => {
+  client.channels.get("489635595551899648").send(`**Woops new server ✅**
 Server name: __${guild.name}__
-Server owner: __${guild.owner}__**)
+Server owner: __${guild.owner}__**`)
 });
    
 client.on('message', message => {
