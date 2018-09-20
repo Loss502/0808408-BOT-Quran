8ac483f46a14f048df6fe5710e47a5cf773b8795
FIKO665 <41846243+FIKO665@users.noreply.github.com>
2018-09-20 22:16:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -672,7 +672,7 @@ client.on('message', message => {
     if (message.author.id === client.user.id) return;
             if (message.content.startsWith(prefix + "ping")) {
         message.channel.sendMessage(' 【 Speed ​​of connection 】 : `' + `${client.ping}` + ' ms`');
-		.setFooter("By : FiKO || ໑ڪــɹ̤ᓅ ")
+	.setFooter("By : FiKO || ໑ڪــɹ̤ᓅ ")
     }
 });
 
